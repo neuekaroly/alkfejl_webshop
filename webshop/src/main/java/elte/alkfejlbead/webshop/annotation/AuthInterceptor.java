a036cff257d3fba7868016d89a988ad9758ca620
neuekaroly <karolyneue6@gmail.com>
2017-11-03 14:45:43 +0100

    Added UserService, UserController and exceptions.

--- webshop/src/main/java/elte/alkfejlbead/webshop/annotation/AuthInterceptor.java
+++ webshop/src/main/java/elte/alkfejlbead/webshop/annotation/AuthInterceptor.java
@@ -1,7 +1,7 @@
 package elte.alkfejlbead.webshop.annotation;
 
 import elte.alkfejlbead.webshop.entity.User;
-import elte.alkfejlbead.webshop.service.SessionService;
+import elte.alkfejlbead.webshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -15,21 +15,25 @@ import java.util.List;
 
 @Component
 public class AuthInterceptor extends HandlerInterceptorAdapter {
+
     @Autowired
-    private SessionService sessionService;
+    private UserService userService;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.err.println("interceptor");
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // handle OPTION methods
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
         List<User.Role> routeRoles = getRoles((HandlerMethod) handler);
-        User user = sessionService.getCurrentUser();
+        User user = userService.getUser();
 
         // when there are no restrictions, we let the user through
         if (routeRoles.isEmpty() || routeRoles.contains(User.Role.GUEST)) {
             return true;
         }
         // check role
-        if (user != null && routeRoles.contains(user.getRole())) {
+        if (userService.isLoggedIn() && routeRoles.contains(user.getRole())) {
             return true;
         }
         response.setStatus(401);
